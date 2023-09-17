1112489752a53113b68a14e08c046e34119d1d08
ian-g123 <igauler@fi.uba.ar>
2023-09-17 16:23:24 -0300

    formateado

--- tp1/main.py
+++ tp1/main.py
@@ -1,24 +1,27 @@
 import argparse
 
-from manejo_archivos import compilados_en_archivo, exportar_compilados
+from manejo_archivos import compilados_en_archivo_a_lista, exportar_compilados
 from algoritmos import compilados_ordenados_de_forma_optima
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Ordena los compilados de manera que se minimice el tiempo total de análisis.')
+    parser = argparse.ArgumentParser(
+        description="Ordena los compilados de manera que se minimice el tiempo total de análisis."
+    )
     parser.add_argument(
-        'archivo_entrada',
-        metavar='compilados_a_ordenar.csv',
+        "archivo_entrada",
+        metavar="compilados_a_ordenar.csv",
         type=open,
         nargs=1,
-        help='Archivo .csv de entrada con el encabezado "S_i,A_i"'
+        help='Archivo .csv de entrada con el encabezado "S_i,A_i"',
     )
 
     parser.add_argument(
-        '-o',
-        dest='archivo_salida',
-        type=argparse.FileType('w',encoding='latin-1'),
-        default='compilados_ordenados.csv',
-        help='Archivos de destino de los compilados ordenados def forma óptima'
+        "-o",
+        dest="archivo_salida",
+        type=argparse.FileType("w", encoding="latin-1"),
+        default="compilados_ordenados.csv",
+        help="Archivos de destino de los compilados ordenados def forma óptima",
     )
 
     args = parser.parse_args()
@@ -26,10 +29,11 @@ def main():
     archivo_entrada = args.archivo_entrada[0].name
     archivo_salida = args.archivo_salida.name
 
-    compilados = compilados_en_archivo(archivo_entrada)
+    compilados = compilados_en_archivo_a_lista(archivo_entrada)
     compilados_ordenados = compilados_ordenados_de_forma_optima(compilados)
 
     exportar_compilados(archivo_salida, compilados_ordenados)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tp1/medicion_tiempo.py
+++ tp1/medicion_tiempo.py
@@ -13,22 +13,24 @@ def tiempo_ejecucion(compilados):
     tiempo_final = time.time()
     return tiempo_final - tiempo_inicial
 
+
 def generar_compilados_aleatorios(cant_compilados):
     compilados = []
     for _ in range(cant_compilados):
         compilados.append(Compilado(random.randint(1, 99999), random.randint(1, 99999)))
     return compilados
 
+
 def main():
-    cantidad_elementos = []
+    cantidad_compilados = []
     tiempos_ejecucion = []
-    
+
     for cantidad in range(1, 10000, 10):
         compilados_ejemplo = generar_compilados_aleatorios(cantidad)
-        cantidad_elementos.append(cantidad)
+        cantidad_compilados.append(cantidad)
         tiempos_ejecucion.append(tiempo_ejecucion(compilados_ejemplo) * 1000)
 
-    x = np.array(cantidad_elementos)
+    x = np.array(cantidad_compilados)
     y = np.array(tiempos_ejecucion)
 
     # Regresión Lineal
@@ -39,22 +41,39 @@ def main():
     # Regresión lineal Logarítmica
     def n_logn_function(x, a):
         return a * x * np.log(x)
-    params, covariance = curve_fit(n_logn_function, x, y)
+
+    params, _ = curve_fit(n_logn_function, x, y)
     a = params[0]
     logaritmic_regression_line = a * x * np.log(x)
 
     # Graficar
     plt.figure(dpi=600)
-    plt.scatter(x, y, label='Tiempo de ejecución', marker='o', color='darkcyan', alpha=0.35, s=4)
-    plt.plot(x, linear_regression_line, label='Regresión Linear', linestyle='-', color='lightcoral', linewidth=2.0)
-    plt.plot(x, logaritmic_regression_line, label='Regresión n log n', linestyle='-', color='navy', linewidth=2.0)
+    plt.scatter(
+        x, y, label="Tiempo de ejecución", marker="o", color="darkcyan", alpha=0.35, s=4
+    )
+    plt.plot(
+        x,
+        linear_regression_line,
+        label="Regresión Linear",
+        linestyle="-",
+        color="lightcoral",
+        linewidth=2.0,
+    )
+    plt.plot(
+        x,
+        logaritmic_regression_line,
+        label="Regresión n log n",
+        linestyle="-",
+        color="navy",
+        linewidth=2.0,
+    )
 
     # Etiquetas y leyenda
-    plt.xlabel('Cantidad de elementos')
-    plt.ylabel('Tiempo de ejecución (ms)')
+    plt.xlabel("Cantidad de compilados")
+    plt.ylabel("Tiempo de ejecución (ms)")
     plt.legend()
-    plt.title('Tiempo de ejecución del algoritmo de ordenamiento')
-    plt.savefig('tiempos_ejecucion.png')
+    plt.title("Tiempo de ejecución del algoritmo de ordenamiento")
+    plt.savefig("tiempos_ejecucion.png")
 
 
 main()
