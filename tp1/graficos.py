e98666ce894604cfc43662e52589ae7ba9820999
patriciotourne <patriciotourne@estudiantesorganizados.ar>
2023-09-26 13:59:42 -0300

    Reentrega 1
    Co-authored-by: Ian Gauler <igauler@fi.uba.ar>
    Co-authored-by: melijauregui <melijauregui@users.noreply.github.com>

---
+++ tp1/graficos.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+import numpy as np
+from algoritmos import Compilado, compilados_ordenados_de_forma_optima
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
