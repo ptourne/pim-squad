2d8e25841e539f2d6982d8de3e7c39f906c4c55c
patriciotourne <patriciotourne@estudiantesorganizados.ar>
2023-09-27 02:37:41 -0300

    Gráficos y demostración
    Co-authored-by: melijauregui <melijauregui@users.noreply.github.com>
    Co-authored-by: Ian Gauler <igauler@fi.uba.ar>

--- tp1/medicion_tiempo.py
+++ tp1/medicion_tiempo.py
@@ -25,111 +25,113 @@ def generar_compilados_aleatorios(cant_compilados):
 
 
 def main():
-    cantidad_compilados = (
-        list(range(1, 2000))
-        + list(range(2000, 10000, 100))
-        + list(range(10000, 100000, 1000))
-        + list(range(10000, 300000, 10000))
-    )
+    cantidades_muestras_compilados = range(1000)
+    muestras_compilados = []
+    for cantidad in cantidades_muestras_compilados:
+        muestras_aux = []
+        for _ in range(3):
+            muestras_aux.append(generar_compilados_aleatorios(cantidad))
+        muestras_compilados.append(muestras_aux)
 
-    tiempos_ejecucion_aux = []
-    for _ in range(PROMEDIO):
-        tiempos_ejecucion_vuelta = [None] * len(cantidad_compilados)
+    print(muestras_compilados)
+    # tiempos_ejecucion_aux = []
+    # for _ in range(PROMEDIO):
+    #     tiempos_ejecucion_vuelta = [None] * len(cantidad_compilados)
         
-        for i, cantidad in enumerate(cantidad_compilados):
-            compilados_ejemplo = generar_compilados_aleatorios(cantidad)
-            tiempos_ejecucion_vuelta[i] = tiempo_ejecucion(compilados_ejemplo) * 1000
-        tiempos_ejecucion_aux.append(tiempos_ejecucion_vuelta)
+    #     for i, cantidad in enumerate(cantidad_compilados):
+    #         compilados_ejemplo = generar_compilados_aleatorios(cantidad)
+    #         tiempos_ejecucion_vuelta[i] = tiempo_ejecucion(compilados_ejemplo) * 1000
+    #     tiempos_ejecucion_aux.append(tiempos_ejecucion_vuelta)
 
 
-    tiempos_ejecucion = [None] * len(cantidad_compilados)
-    for j in range(len(cantidad_compilados)):
-        tiempos_ejecucion_cantidad = []
-        for i in range(PROMEDIO):
-            tiempos_ejecucion_cantidad.append(tiempos_ejecucion_aux[i][j])
-        tiempos_ejecucion[j] = np.mean(tiempos_ejecucion_cantidad)
+    # tiempos_ejecucion = [None] * len(cantidad_compilados)
+    # for j in range(len(cantidad_compilados)):
+    #     tiempos_ejecucion_cantidad = []
+    #     for i in range(PROMEDIO):
+    #         tiempos_ejecucion_cantidad.append(tiempos_ejecucion_aux[i][j])
+    #     tiempos_ejecucion[j] = np.mean(tiempos_ejecucion_cantidad)
 
 
 
-    # for i, cantidad in enumerate(cantidad_compilados):
-    #     compilados_ejemplo = generar_compilados_aleatorios(cantidad)
-    #     tiempos_ejecucion[i] = tiempo_ejecucion(compilados_ejemplo) * 1000
+    # # for i, cantidad in enumerate(cantidad_compilados):
+    # #     compilados_ejemplo = generar_compilados_aleatorios(cantidad)
+    # #     tiempos_ejecucion[i] = tiempo_ejecucion(compilados_ejemplo) * 1000
 
 
-    x = np.array(cantidad_compilados)
-    y = np.array(tiempos_ejecucion)
+    # x = np.array(cantidad_compilados)
+    # y = np.array(tiempos_ejecucion)
 
-    # Regresión Lineal
-    coefficients = np.polyfit(x, y, 1)
-    slope, intercept = coefficients
-    linear_regression_line = slope * x + intercept
+    # # Regresión Lineal
+    # coefficients = np.polyfit(x, y, 1)
+    # slope, intercept = coefficients
+    # linear_regression_line = slope * x + intercept
 
-    # Calcular el RMSE (Root mean square error) para la regresión lineal
-    rmse_recta_lineal = np.sqrt(mean_squared_error(y, linear_regression_line))
-    print(f"RMSE para la recta lineal: {rmse_recta_lineal}")
+    # # Calcular el RMSE (Root mean square error) para la regresión lineal
+    # rmse_recta_lineal = np.sqrt(mean_squared_error(y, linear_regression_line))
+    # print(f"RMSE para la recta lineal: {rmse_recta_lineal}")
 
-    # Regresión lineal Logarítmica
-    def n_logn_function(x, a):
-        return a * x * np.log(x)
+    # # Regresión lineal Logarítmica
+    # def n_logn_function(x, a):
+    #     return a * x * np.log(x)
 
-    params, _ = curve_fit(n_logn_function, x, y)
-    a = params[0]
-    logaritmic_regression_line = a * x * np.log(x)
+    # params, _ = curve_fit(n_logn_function, x, y)
+    # a = params[0]
+    # logaritmic_regression_line = a * x * np.log(x)
 
-    # Calcular el RMSE (Root mean square error) para la regresión lineal logarítmica
-    rmse_funcion_lineal_log = np.sqrt(mean_squared_error(y, logaritmic_regression_line))
-    print(f"RMSE para la función lineal logarítmica: {rmse_funcion_lineal_log}")
+    # # Calcular el RMSE (Root mean square error) para la regresión lineal logarítmica
+    # rmse_funcion_lineal_log = np.sqrt(mean_squared_error(y, logaritmic_regression_line))
+    # print(f"RMSE para la función lineal logarítmica: {rmse_funcion_lineal_log}")
 
-    # Graficar
-    plt.figure(dpi=600)
-    plt.scatter(
-        x, y, label="Tiempo de ejecución", marker="o", color="darkcyan", alpha=0.35, s=4
-    )
-    plt.plot(
-        x,
-        linear_regression_line,
-        label="Regresión Linear",
-        linestyle="-",
-        color="lightcoral",
-        linewidth=2.0,
-    )
-    plt.plot(
-        x,
-        logaritmic_regression_line,
-        label="Regresión n log n",
-        linestyle="-",
-        color="navy",
-        linewidth=2.0,
-    )
+    # # Graficar
+    # plt.figure(dpi=600)
+    # plt.scatter(
+    #     x, y, label="Tiempo de ejecución", marker="o", color="darkcyan", alpha=0.35, s=4
+    # )
+    # plt.plot(
+    #     x,
+    #     linear_regression_line,
+    #     label="Regresión Linear",
+    #     linestyle="-",
+    #     color="lightcoral",
+    #     linewidth=2.0,
+    # )
+    # plt.plot(
+    #     x,
+    #     logaritmic_regression_line,
+    #     label="Regresión n log n",
+    #     linestyle="-",
+    #     color="navy",
+    #     linewidth=2.0,
+    # )
 
-    # Anotamos el rmse de cada regresión
-    plt.annotate(
-        f"RMSE recta lineal: {rmse_recta_lineal:.5f}",
-        (0.02, 0.75),
-        xycoords="axes fraction",
-        fontsize=8,
-        color="lightcoral",
-    )
+    # # Anotamos el rmse de cada regresión
+    # plt.annotate(
+    #     f"RMSE recta lineal: {rmse_recta_lineal:.5f}",
+    #     (0.02, 0.75),
+    #     xycoords="axes fraction",
+    #     fontsize=8,
+    #     color="lightcoral",
+    # )
 
-    plt.annotate(
-        f"RMSE n log n: {rmse_funcion_lineal_log:.5f}",
-        (0.02, 0.70),
-        xycoords="axes fraction",
-        fontsize=8,
-        color="navy",
-    )
+    # plt.annotate(
+    #     f"RMSE n log n: {rmse_funcion_lineal_log:.5f}",
+    #     (0.02, 0.70),
+    #     xycoords="axes fraction",
+    #     fontsize=8,
+    #     color="navy",
+    # )
 
-    # Etiquetas y leyenda
-    plt.xlabel("Cantidad de compilados")
-    plt.ylabel("Tiempo de ejecución (ms)")
-    plt.legend()
-    plt.title("Tiempo de ejecución del algoritmo de ordenamiento")
-    plt.savefig("tiempos_ejecucion.png")
+    # # Etiquetas y leyenda
+    # plt.xlabel("Cantidad de compilados")
+    # plt.ylabel("Tiempo de ejecución (ms)")
+    # plt.legend()
+    # plt.title("Tiempo de ejecución del algoritmo de ordenamiento")
+    # plt.savefig("tiempos_ejecucion.png")
 
-    # Ahora guardamos el gráfico con un zoom en los primeros valores para notar una "panza" en la función n log n
-    plt.xlim(0, 2000)
-    plt.ylim(0, 0.8)
-    plt.savefig("tiempos_ejecucion_zoom.png")
+    # # Ahora guardamos el gráfico con un zoom en los primeros valores para notar una "panza" en la función n log n
+    # plt.xlim(0, 2000)
+    # plt.ylim(0, 0.8)
+    # plt.savefig("tiempos_ejecucion_zoom.png")
 
 
 main()
